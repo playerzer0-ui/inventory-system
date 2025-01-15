c98e1442af933c296d84b0fba3c87148ecb1c653
player_zero <playerzero745@gmail.com>
2025-01-15 08:53:48 +0000

    database intial made

---
+++ database/migrations/2025_01_15_074018_initial.php
@@ -0,0 +1,140 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->uuid("userID")->primary();
+            $table->string("email", 255);
+            $table->string("password", 255);
+            $table->integer("userType");
+            }
+        );
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->string("productCode", 5)->primary();
+            $table->string("productName", 100);
+            }
+        );
+
+        Schema::create('customers', function (Blueprint $table) {
+            $table->string("customerCode", 5)->primary();
+            $table->string("customerName", 100);
+            $table->string("customerAddress", 100);
+            $table->string("customerNPWP", 100);
+            }
+        );
+
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->string("vendorCode", 5)->primary();
+            $table->string("vendorName", 100);
+            $table->string("vendorAddress", 100);
+            $table->string("vendorNPWP", 100);
+            }
+        );
+
+        Schema::create('storages', function (Blueprint $table) {
+            $table->string("storageCode", 5)->primary();
+            $table->string("storageName", 100);
+            $table->string("storageAddress", 100);
+            $table->string("storageNPWP", 100);
+            }
+        );
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->string("nomor_surat_jalan", 100)->primary();
+            $table->string("storageCode", 5);
+            $table->string("no_LPB", 5);
+            $table->string("no_truk", 5);
+            $table->string("vendorCode", 5);
+            $table->string("customerCode", 5);
+            $table->dateTime("orderDate")->nullable();
+            $table->string("purchase_order", 30);
+            $table->integer("status_mode");
+            $table->foreign('storageCode')->references('storageCode')->on('storages')->onDelete('cascade');
+            $table->foreign('vendorCode')->references('vendorCode')->on('vendors')->onDelete('cascade');
+            $table->foreign('customerCode')->references('customerCode')->on('customers')->onDelete('cascade');
+            }
+        );
+
+        Schema::create('invoices', function(Blueprint $table){
+            $table->string("nomor_surat_jalan", 100);
+            $table->dateTime("invoice_date");
+            $table->string("no_invoice", 100);
+            $table->string("no_faktur", 100);
+            $table->string("no_moving", 100);
+            $table->double("tax");
+            $table->foreign('nomor_surat_jalan')->references('nomor_surat_jalan')->on('orders')->onDelete('cascade');
+        });
+
+        Schema::create('payments', function(Blueprint $table){
+            $table->string("nomor_surat_jalan", 100);
+            $table->dateTime("payment_date");
+            $table->double("payment_amount");
+            $table->foreign('nomor_surat_jalan')->references('nomor_surat_jalan')->on('orders')->onDelete('cascade');
+        });
+
+        Schema::create('repacks', function(Blueprint $table){
+            $table->string("no_repack", 100)->primary();
+            $table->dateTime("repack_date")->nullable();
+            $table->string("storageCode", 5);
+            $table->foreign('storageCode')->references('storageCode')->on('storages')->onDelete('cascade');
+        });
+
+        Schema::create('movings', function(Blueprint $table){
+            $table->string("no_moving", 100)->primary();
+            $table->dateTime("moving_date")->nullable();
+            $table->string("storageCodeSender", 5);
+            $table->string("storageCodeReceiver", 5);
+            $table->foreign('storageCodeSender')->references('storageCode')->on('storages')->onDelete('cascade');
+            $table->foreign('storageCodeReceiver')->references('storageCode')->on('storages')->onDelete('cascade');
+        });
+
+        Schema::create('saldos', function(Blueprint $table){
+            $table->string("productCode", 5);
+            $table->string("storageCode", 5);
+            $table->integer("totalQty");
+            $table->double("price_per_qty");
+            $table->integer("saldoMonth");
+            $table->integer("saldoYear");
+            $table->integer("saldoCount");
+            $table->foreign('productCode')->references('productCode')->on('products')->onDelete('cascade');
+            $table->foreign('storageCode')->references('storageCode')->on('storages')->onDelete('cascade');
+        });
+
+        Schema::create('order_products', function(Blueprint $table){
+            $table->string("nomor_surat_jalan", 100);
+            $table->string("repack_no_repack", 100);
+            $table->string("moving_no_moving", 100);
+            $table->string("productCode", 5);
+            $table->integer("qty");
+            $table->string("UOM", 20);
+            $table->double("price_per_UOM");
+            $table->string("product_status", 10);
+            $table->foreign('nomor_surat_jalan')->references('nomor_surat_jalan')->on('orders')->onDelete('cascade');
+            $table->foreign('repack_no_repack')->references('no_repack')->on('repacks')->onDelete('cascade');
+            $table->foreign('moving_no_moving')->references('no_moving')->on('movings')->onDelete('cascade');
+            $table->foreign('productCode')->references('productCode')->on('products')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
---
+++ database/seeders/NeutralValues.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class NeutralValues extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::insert('insert into storages values (?, ?, ?, ?)', ["NON", "none", "none", "001.111.111.111-111"]);
+        DB::insert('insert into vendors values (?, ?, ?, ?)', ["NON", "none", "none", "001.111.111.111-111"]);
+        DB::insert('insert into customers values (?, ?, ?, ?)', ["NON", "none", "none", "001.111.111.111-111"]);
+
+        DB::insert('insert into orders values (?, ?, ?, ?, ?, ?, ?, ?, ?)', ["-", "NON", "-", "-", "NON", "NON", null, "-", "0"]);
+        DB::insert('insert into repacks values (?, ?, ?)', ["-", null, "NON"]);
+        DB::insert('insert into movings values (?, ?, ?, ?)', ["-", null, "NON", "NON"]);
+        
+        DB::table('users')->insert([
+            [
+                'userID' => '37d72912-5ad0-11ef-b5d1-5cbaef99b658',
+                'email' => 'playerzero745@gmail.com',
+                'password' => '$2y$10$I6HDp20xfQ.eyexX6Xu0XOmiCwmPmVGf7WuNTF6LApGFg0kxVcbIG',
+                'userType' => 1,
+            ],
+            [
+                'userID' => '3de53767-5ad0-11ef-b5d1-5cbaef99b658',
+                'email' => 'cydacnote@gmail.com',
+                'password' => '$2y$10$5Ymv4R2Qn3Fw/8FKRJxHmu5XAmO2G0mfXTK2naenRcsssZuvPBLVa',
+                'userType' => 0,
+            ],
+        ]);
+    }
+}
