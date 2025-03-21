d446c4f5d074e740799fdc62d841665552ce9859
player_zero <playerzero745@gmail.com>
2025-03-21 09:21:13 +0000

    cloud update, everything become one

--- cloud/setup.ps1
+++ cloud/setup.ps1
@@ -1,6 +1,8 @@
 # Variables
 $resourcegroup = "ca1_rg"
 $vmname = "web-vm"
+$templateFile = "template.json"       # Path to the template.json file
+$parametersFile = "parameters.json"
 $location = "westeurope"
 $sshKeyPath = "$env:USERPROFILE\.ssh\id_rsa.pub"  # Path to your public key
 
@@ -12,6 +14,19 @@ if ($exists -eq $true) {
     Write-Host "Creating resource group..."
     az group create -n $resourcegroup -l $location
 }
+
+Write-Host "deploy the mails! " -ForegroundColor Yellow
+az deployment group create --resource-group $resourcegroup --template-file $templateFile --parameters $parametersFile
+
+$domain = az communication email domain list --email-service-name "mail-system" --resource-group $resourcegroup | ConvertFrom-Json
+
+$json = az communication list-key -n "com-system" --resource-group $resourcegroup | ConvertFrom-Json
+$conn = $json.primaryConnectionString -split ";"
+
+$endpoint = $conn[0] -replace "endpoint=", ""
+$accessKey = $conn[1] -replace "accesskey=", ""
+
+$email = "DoNotReply@$($domain[0].fromSenderDomain)"
 
 # Create Public IP
 Write-Host "Creating Public IP..." -ForegroundColor DarkYellow
@@ -43,13 +58,16 @@ Start-Sleep -Seconds 30
 
 # Get the assigned public IP
 $publicIp = az vm show -d -g $resourcegroup -n $vmname --query publicIps -o tsv
-Write-Host "VM Public IP: $publicIp" -ForegroundColor Green
 
 # Copy files to VM
 Write-Host "Copying files to VM..." -ForegroundColor DarkYellow
 scp -i "$env:USERPROFILE\.ssh\id_rsa" .\vm_playbook.yml developer@${publicIp}:/home/developer/
 scp -i "$env:USERPROFILE\.ssh\id_rsa" .\inventory.ini developer@${publicIp}:/home/developer/
 
+Write-Host "VM Public IP: $publicIp" -ForegroundColor Green
+Write-Host "endpoint: $endpoint" -ForegroundColor Green 
+Write-Host "accessKey: $accessKey" -ForegroundColor Green 
+Write-Host "email: $email" -ForegroundColor Green 
 # SSH into the VM
 # Write-Host "Connecting to VM via SSH..." -ForegroundColor Cyan
 # ssh -i "$env:USERPROFILE\.ssh\id_rsa" developer@$publicIp
--- cloud/teardown.ps1
+++ cloud/teardown.ps1
@@ -1,2 +1,1 @@
 az group delete -n ca1_rg
-az group delete -n mail_rg
